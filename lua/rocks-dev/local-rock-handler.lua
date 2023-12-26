cb29db92497b755733c644b91f53848f43cb169e
Vhyrro <vhyrro@gmail.com>
2023-12-26 15:29:50 +0100

    feat: better rock handling

--- lua/rocks-dev/local-rock-handler.lua
+++ lua/rocks-dev/local-rock-handler.lua
@@ -9,10 +9,17 @@ local rock_handler = {}
 ---@param rock RockSpec
 function rock_handler.get_sync_callback(rock)
     if rock.dir then
-        return function()
+        return function(report_progress, report_error)
             api.query_installed_rocks(function(rocks)
                 if rocks[rock.name] then
-                    operations.prune(rock.name)
+                    local ok = pcall(operations.remove(rock.name, report_progress).wait)
+
+                    if not ok then
+                        report_error(("rocks-dev: Failed to remove %s"):format(rock.name))
+                        return
+                    end
+
+                    report_progress(("rocks-dev: Hotswapped %s"):format(rock.name))
                 end
             end)
         end
--- plugin/rocks-dev.lua
+++ plugin/rocks-dev.lua
@@ -9,8 +9,8 @@ Example rocks.toml:
 local ok, api = pcall(require, "rocks.api")
 
 if not ok then
-    vim.notify("[rocks-dev.nvim]: rocks.nvim not found, aborting!", vim.log.levels.ERROR, {})
-    return
+	vim.notify("[rocks-dev.nvim]: rocks.nvim not found, aborting!", vim.log.levels.ERROR, {})
+	return
 end
 
 local user_configuration = api.get_rocks_toml()
