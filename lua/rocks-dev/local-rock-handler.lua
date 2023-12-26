a73978c8daadf3a92ecef5acd5477f9758e2936f
Vhyrro <vhyrro@gmail.com>
2023-12-26 15:19:11 +0100

    fix: don't remove plugins which don't exist

--- lua/rocks-dev/local-rock-handler.lua
+++ lua/rocks-dev/local-rock-handler.lua
@@ -1,6 +1,7 @@
 ---@alias rock_name string
 ---@class RockSpec: { name: rock_name, version?: string, [string]: any }
 
+local api = require("rocks.api")
 local operations = require("rocks.operations")
 
 local rock_handler = {}
@@ -9,16 +10,16 @@ local rock_handler = {}
 function rock_handler.get_sync_callback(rock)
     if rock.dir then
         return function()
-            return operations.prune(rock.name)
+            api.query_installed_rocks(function(rocks)
+                if rocks[rock.name] then
+                    operations.prune(rock.name)
+                end
+            end)
         end
     end
 end
 
----@param rock RockSpec
-function rock_handler.get_prune_callback(rock)
-    if rock.dir then
-        return operations.prune(rock.name)
-    end
+function rock_handler.get_prune_callback()
 end
 
 return rock_handler
